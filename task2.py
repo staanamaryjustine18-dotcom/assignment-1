f0d3e9ce0254be880201a7c03ae404d784ad8b27
staanamaryjustine18-dotcom <staana.maryjustine18@gmail.com>
2026-02-11 17:19:00 +0800

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,16 @@
+fullname = "Mary Justine"
+age = 19
+height = 1.55
+currently_working = False
+
+print("fullname:", fullname)
+print(type(fullname))
+      
+print("\nAge:", age)
+print(type(age))
+
+print("\nHeight:", height)
+print(type(height))
+
+print("\nCurrently working:", currently_working)
+print(type(currently_working))
